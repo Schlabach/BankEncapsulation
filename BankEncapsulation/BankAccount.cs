e8d704a48430645924416f2c3615b9a180ee92d2
Schlabach <matthewshawn89@gmail.com>
2023-01-04 07:15:03 -0500

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public BankAccount()
+        {
+        }
+
+        public void Deposit(double depositAmount)
+        {
+            balance += depositAmount;
+            Console.WriteLine($"{depositAmount} has been deposited into your account.");
+        
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"${balance}");
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine($"How much money would you like to deposit into your account?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+
+            List<string> response = new List<string> { "yes", "Yes", "yea", "Yea", "yep", "Yep" };
+
+            Console.WriteLine($"Would you like to see your account balance?");
+            string answer = Console.ReadLine();
+            if(!response.Contains(answer))
+            {
+                Console.WriteLine($"Have a nice day!");
+            }
+            else
+            {
+                account.GetBalance();
+            }
+
+
         }
+
+
     }
 }
